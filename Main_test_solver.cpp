5d3561005789fb18c94f5218b56513dbe869c0bb
acse-abm15 <56032215+acse-abm15@users.noreply.github.com>
2020-01-31 15:33:40 +0000

    Update Main_test_solver.cpp

--- Main_test_solver.cpp
+++ Main_test_solver.cpp
@@ -1,6 +1,3 @@
-// Sparse_Matrix.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "CSRMatrix.h"
 #include "CSRMatrix.cpp"
@@ -14,10 +11,52 @@
 
 int main()
 {
-	/////Check for memory leaks but on windows only
-	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-    //srand(time(NULL));
+	////////THIS PART OF CODE GENERATES 50x50 DENSE MATRIX M AND VECTOR B AND THEN SOLVES THE EQUATION M*x=b
+///===============================================================================================================
+	///We will create a random dense matrix
+	Test<double> TestMatrix;
+	//matrix size will be 50x50
+	TestMatrix.rows = 50;
+	TestMatrix.cols = 50;
+	//the values on the diagonal will be between 20 and 40, all other values will be between 0 and 1
+	//the matrix will be symmetric
+	TestMatrix.DIAG_MAX = 40;
+	TestMatrix.DIAG_MIN = 20;
+	//create a matrix object
+	auto* DenseMatrix = new Matrix   <double>(TestMatrix.rows, TestMatrix.cols, true);
+	//use functon from tests library to fill DenseMatrix with random values, described above
+	TestMatrix.Generate_Dense_Matrix(*DenseMatrix);
+	//create vector B
+	auto* Vector_B = new Matrix   <double>(TestMatrix.rows, 1, true);
+	//use functon from tests library to fill Vector_B with random values
+	TestMatrix.Generate_Random_Vector(*Vector_B);
+	//create vector to write out the result
+	auto* Vector_X = new Matrix   <double>(TestMatrix.rows, 1, true);
+	//use functon from tests library to fill with zeros for now
+	TestMatrix.Initialize_Empty_Vector(*Vector_X);
+    
+	//call the solver method on dense matrix to solve the equation M*x=b
+	//here we use LU solver, there are many others avaiable,just change the last parameter
+	//to one of: Jacobi, Gauss_Siedel, Gaussian, LU, Inverse, Cholesky, Conjugate_Gradient, Gauss_Jordan,Cramers
+	DenseMatrix->solve(*Vector_B, *Vector_X, Gauss_Jordan);
 
+	//now we will do some tests to check if the result is correct
+
+	//create a new empty vector
+	auto* Test_Vector = new Matrix   <double>(TestMatrix.rows, 1, true);
+
+	//now multipy calculated x_vector and original matrix
+	//it should equal  to original b vector (to a given tolerance)
+	DenseMatrix->matVecMult(*Vector_X, *Test_Vector);
+
+	std::cout << "Lets check what is the difference orignal B vector and calculated_B_vector: \n";
+	double difference = TestMatrix.Compare_Two_Matrices(*Test_Vector, *Vector_B);
+	
+	delete DenseMatrix;
+	delete Vector_B;
+	delete Vector_X;
+	delete Test_Vector;
+///===============================================================================================================
 	///////////Example usage of solver for used-defined matrices
 	////////////////////////////////////////////////////////////
 	//Uncomment block below to see example user usage
@@ -64,22 +103,22 @@ int main()
 	delete vector_b;
 	delete vector_out;
 	delete vector_out2;
-	////////////////////////////////////////////////////////////
 	*/
+///===============================================================================================================
 	///////////UNCOMMENT BLOCK OF CODE BELOW TO PERFORM TESTS ON DENSE SOLVERS
-	
+	/*
     Test<double> General_Solver;
     std::cout << "Test for Dense solvers:\n";
     General_Solver.Run_Test(verb_0, General_Solver_Test,All_Dense);
-	
-
+	*/
+///===============================================================================================================
 	/////////UNCOMMENT BLOCK OF CODE BELOW TO PERFORM TESTS ON SPARSE SOLVERS
-    
+    /*
     Test<double> Sparse_Solver;
 	std::cout << "Test for Sparse solvers:\n";
     Sparse_Solver.Run_Test(verb_0, CSR_Solver_Test, All_Sparse);
-	
-
+	*/
+///===============================================================================================================
 	////////UNCOMMENT BLOCK OF CODE BELOW TO PERFORM TIMING TESTS ON SPARSE MATRICES
 	////////RESULTS WILL BE WRITTEN TO THE FILE
 	////////the matrices will be originally generated as SPARSE CSR matrices
@@ -88,7 +127,7 @@ int main()
 	Test<double> Timing_Test_Sparse;
 	Timing_Test_Sparse.Time_Solvers_Sparse();
 	*/
-
+///===============================================================================================================
 	////////UNCOMMENT BLOCK OF CODE BELOW TO PERFORM TIMING TESTS ON DENSE MATRICES
 	////////RESULTS WILL BE WRITTEN TO THE FILE
 	///////the matrices will be originally generated as DENSE CSR matrices
@@ -97,7 +136,6 @@ int main()
 	Test<double> Test_Timing_Dense;
 	Test_Timing_Dense.Time_Solvers_Dense();
 	*/
+///===============================================================================================================
 
-	//stop but for windows only
-    //system("PAUSE"); 
 }
