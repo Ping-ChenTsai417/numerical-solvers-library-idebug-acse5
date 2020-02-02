5bb9f93b9d4633ccae5bc406d9bf5fe540450637
acse-abm15 <56032215+acse-abm15@users.noreply.github.com>
2020-02-02 21:03:50 +0000

    Update Main_test_solver.cpp

--- Main_test_solver.cpp
+++ Main_test_solver.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-#include "CSRMatrix.h"
-#include "CSRMatrix.cpp"
-#include "Matrix.h"
-#include "Matrix.cpp"
 #include <stdlib.h>    
 #include <time.h> 
 #include <string>
+#include "Matrix.h"
+#include "Matrix.cpp"
+#include "CSRMatrix.h"
+#include "CSRMatrix.cpp"
 #include "Test.h"
 #include "Test.cpp"
+#include "BandedMatrix.h"
+#include "BandedMatrix.cpp"
 
 int main()
+
 {
+
 	////////THIS PART OF CODE GENERATES 50x50 DENSE MATRIX M AND VECTOR B AND THEN SOLVES THE EQUATION M*x=b
 ///===============================================================================================================
 	///We will create a random dense matrix
@@ -113,7 +117,7 @@ int main()
 	*/
 ///===============================================================================================================
 	/////////UNCOMMENT BLOCK OF CODE BELOW TO PERFORM TESTS ON SPARSE SOLVERS
-    /*
+	/*
     Test<double> Sparse_Solver;
 	std::cout << "Test for Sparse solvers:\n";
     Sparse_Solver.Run_Test(verb_1, CSR_Solver_Test, All_Sparse);
@@ -123,6 +127,7 @@ int main()
 	////////RESULTS WILL BE WRITTEN TO THE FILE
 	////////the matrices will be originally generated as SPARSE CSR matrices
 	////////and then converted to normal sparse matrices for dense solvers
+	//////tests will be performed on matrices size 10x10 to 200x200
 	/*
 	Test<double> Timing_Test_Sparse;
 	Timing_Test_Sparse.Time_Solvers_Sparse();
@@ -132,10 +137,62 @@ int main()
 	////////RESULTS WILL BE WRITTEN TO THE FILE
 	///////the matrices will be originally generated as DENSE CSR matrices
 	///////and then converted to normal dense matrices for dense solvers
+	//////tests will be performed on matrices size 10x10 to 200x200
 	/*
 	Test<double> Test_Timing_Dense;
 	Test_Timing_Dense.Time_Solvers_Dense();
 	*/
 ///===============================================================================================================
+
+//==========================================================================================================
+	////////Banded Class Example Output
+	////////PLEASE NOTE THAT THIS CLASS IS NOT FINISHED
+	///////it was included here to show that the effort has been made to include banded matrix class in our library
+	///////UNCOMMENT BLOCK OF CODE BELOW TO USE IT
+	/*
+	//Banded Class
+	int nnzs2 = 22;
+	int rows2 = 8;
+	int cols2 = 8;
+	double mat_val2[22]{ 1, 3, 3, 4, 6, 6, 4, 8, 1, 1, 2, 3, 1, 5, 6, 7, 2, 2, 1, 6, 2, 4 };
+	double IA2[9]{ 0, 2, 6, 9, 11, 14, 17, 19, 22 };
+	double JA2[22]{ 0, 4, 1, 2, 5, 7, 1, 2, 4, 3, 6, 0, 2, 4, 1, 5, 7, 3, 6, 1, 5, 7 };
+
+	auto* banded_mat = new BandedMatrix<double>(rows2, cols2, nnzs2, true);
+	for (int i = 0; i < nnzs2; i++)
+	{
+		banded_mat->values[i] = mat_val2[i];
+		banded_mat->col_index[i] = JA2[i];
+	}
+	for (int i = 0; i < rows2 + 1; i++)
+	{
+		banded_mat->row_position[i] = IA2[i];
+	}
+	cout << endl<< "Example output for Banded Class:" << endl;
+	cout << endl << "============================Print Original Sparse Matrix============================" << endl;
+	banded_mat->printBandedMatrix();
+
+	// Do not run the following line , since Cuthill_Mckee() has bugs inside.
+	// banded_mat->Run_Reverse_Cuthill_Mckee();
+
+
+	// However, other functions works well
+
+	// Get the degree from the adjacency matrix
+	vector<double> degree = banded_mat->get_degree();
+	cout << endl << "Print node degree: ";
+	for (auto i : degree)
+		cout << i << ' ';
+	cout << endl;
+
+
+	// Assume Cuthill_Mckee() works perfectly, then R is computed correcltly
+	// See BandedMatrix.h for initialising vector R.   ----->    vector<double> R{ 0, 4, 2, 1, 5, 7, 3, 6 };
+	banded_mat->reverse_R();
+	banded_mat->permutate_to_Band();
+
+	delete banded_mat;
+	*/
+//===============================================================================================================
 
 }
